5871b42e6d74e41e337e2d7298d98165ff7b41a5
AlfredoIFP <AlfredoIFP@10.0.2.4>
2022-04-01 00:20:34 +0200

    Cambio de variables y comentarios

--- src/practica1/notas.java
+++ src/practica1/notas.java
@@ -12,10 +12,10 @@ import java.util.Scanner;
  * Muestra si es aprobado o suspenso.
  */
 public class notas {
-	//declaramos la variables que nos hacen falta
-	double uf1, uf2, uf3;
-	double acu1, acu2, acu3, def;
-	//utilizames escaner para poder introducir datos
+	//declaración de variables de clase
+	double nota1, nota2, nota3;
+	double notaAcumulada1, notaAcumulada2, notaAcumulada3, notaDefinitiva;
+	//Scanner
 	Scanner entrada = new Scanner(System.in);
 	
 
@@ -24,21 +24,21 @@ public class notas {
 	 * Método que pide al usuario ingresar la nota 1, 2 y 3 para almacenarlas en las variables de clase. 
 	 */
 	public void IngresaNotas() {
-		//cuando ejeuctamos este metodo lo primnero que queremos es que nos pida notas
+		//Pedir notas para almacenar en las variables de clase
 		System.out.println("ingrese las notas del estudiante");
 		
 		System.out.print("ingrese nota 1: ");
 		
-		uf1= entrada.nextDouble();
+		nota1= entrada.nextDouble();
 
 		
 		
 		System.out.print("ingrese nota 2: ");
 		
-		uf2= entrada.nextDouble();
+		nota2= entrada.nextDouble();
 		System.out.print("ingrese nota 3: ");
 		
-		uf3= entrada.nextDouble();
+		nota3= entrada.nextDouble();
 		
 	
 	}
@@ -49,20 +49,20 @@ public class notas {
 	 */
 	public void comprobarcion(){
 		
-		if (uf1>10) {
+		if (nota1>10) {
 			System.out.println(" nota1 mal introducida");
 			
 		}else {
 			System.out.println(" nota1 correcta");
 		}
 		
-		if (uf2>10) {
+		if (nota2>10) {
 			System.out.println(" nota2 mal introducida");
 			
 		}else {
 			System.out.println(" nota2 correcta");
 		}
-		if (uf3>10) {
+		if (nota3>10) {
 			System.out.println(" nota3 mal introducida");
 			
 		}else {
@@ -79,30 +79,30 @@ public class notas {
 	 * Método para calcular la nota final
 	 */
 	public void Calculonotas() {
-		acu1= uf1*0.35;
-		acu2 = uf2 * 0.35;
-		acu3 = uf3 * 0.30;
+		notaAcumulada1= nota1*0.35;
+		notaAcumulada2 = nota2 * 0.35;
+		notaAcumulada3 = nota3 * 0.30;
 		
-		def = acu1 + acu2+ acu3;
+		notaDefinitiva = notaAcumulada1 + notaAcumulada2+ notaAcumulada3;
 		
-		//hasta aqui la tenemos calculada pero no la mostramos
+		//Guardamos la nota definitiva
 	}
 	
 	/**
 	 * Método para mostrar las notas, el acumulado y la nota definitiva
 	 */
 	public void Mostrar() {
-		//print ln lo que hace es que al terminar el print baja la linea
+		//Se imprimen los resultados
 		System.out.println(" notas introducidas son:");
-		System.out.println(" nota1 = " + uf1);
-		System.out.println(" nota2 = " + uf2);
-		System.out.println(" nota3 = " + uf3);
+		System.out.println(" nota 1 = " + nota1);
+		System.out.println(" nota 2 = " + nota2);
+		System.out.println(" nota 3 = " + nota3);
 		
-		System.out.println(" acumuado 1 = "+ acu1);
-		System.out.println(" acumuado 2 = "+ acu2);
-		System.out.println(" acumuado 3 = "+ acu3);
+		System.out.println(" acumulado 1 = "+ notaAcumulada1);
+		System.out.println(" acumulado 2 = "+ notaAcumulada2);
+		System.out.println(" acumulado 3 = "+ notaAcumulada3);
 		
-		System.out.println(" nota definitiva es = "+ def);
+		System.out.println(" nota definitiva es = "+ notaDefinitiva);
 		
 	}
 
@@ -113,10 +113,10 @@ public class notas {
 	 */
 	public void aprobado() {
 			
-			if(def<5 && def>=0) {
+			if(notaDefinitiva<5 && notaDefinitiva>=0) {
 				System.out.println("suspendio");
 			}else {
-				if (def>=5 && def<=10 ) {
+				if (notaDefinitiva>=5 && notaDefinitiva<=10 ) {
 				System.out.println("aprobado");
 				}else {
 					System.out.println(" error en la notas");
@@ -126,9 +126,10 @@ public class notas {
 		
 		
 	 public static void main(String[] args) {
-		// creamos mecanimos para llamar a cualquier metodo fuero de la clase
+		// Creación de la clase notas
 		notas fc= new notas();
 		
+		// Llamada a sus métodos
 		fc.IngresaNotas();
 		
 		fc.comprobarcion();
